fe9c551846098a0606809daba07ee2d94adbc1fb
RRichard2004 <ricsiruszo90@gmail.com>
2024-06-07 17:43:56 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.eduappdev',
+  appName: 'eduappdev',
+  webDir: 'dist'
+};
+
+export default config;
